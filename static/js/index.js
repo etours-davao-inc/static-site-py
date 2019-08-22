b9ec0520353d96b85b9240996e8ea1d811956591
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-08-22 10:14:36 +0800

    Add lazyloading of youtube video

--- static/js/index.js
+++ static/js/index.js
@@ -13,9 +13,28 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener('load', () => {
     let videoBanner = document.getElementById("videoBanner");
     let video = document.createElement("source");
-    video.src = "https://res.cloudinary.com/etours-davao/video/upload/v1564969038/etours-davao-kadayawan-2019-c_scale_vc_auto_w_992_kly8v3.mp4";
+    let youtube = document.querySelectorAll(".youtube");
+    // video.src = "https://res.cloudinary.com/etours-davao/video/upload/v1564969038/etours-davao-kadayawan-2019-c_scale_vc_auto_w_992_kly8v3.mp4";
     video.type = "video/mp4";
     videoBanner.appendChild(video);
+
+    youtube.forEach((element) => {
+      let source = "https://img.youtube.com/vi/"+ element.dataset.embed +"/sddefault.jpg";
+      let image = new Image();
+      image.src = source;
+      element.appendChild(image);
+
+      element.addEventListener('click', () => {
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute( "frameborder", "0" );
+        iframe.setAttribute( "allowfullscreen", "" );
+        iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ element.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+        element.innerHTML = "";
+        element.appendChild(iframe);
+      })
+
+    })
+
   });
 
   function observeLazyImages(lazyImages) {
